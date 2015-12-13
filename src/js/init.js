e3514cf507b17c91cab783bad509b22266e9161c
Jun <felipe.jun.ohira@gmail.com>
2015-12-13 12:04:38 -0200

    Adding watchify

--- src/js/init.js
+++ src/js/init.js
@@ -1,10 +1,1 @@
-var MenuController = require('./controllers/menu_controller');
 
-var menuItems = [
-  { title: 'Teste 1' },
-  { title: 'Teste 2' },
-  { title: 'Teste 3' },
-  { title: 'Teste 4' }
-]
-
-var controller = new MenuController(menuItems);
