61f7edbfaa4d3f321680af02a502fb7a1883cb2a
Jun <felipe.jun.ohira@gmail.com>
2015-12-15 23:38:31 -0200

    Getting karma module config from webpack config file

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration
 // Generated on Tue Dec 15 2015 23:01:37 GMT-0200 (BRST)
 
+var webpackConfig = require('./webpack.config');
+
 module.exports = function(config) {
   config.set({
     browsers: ['Chrome'],
@@ -15,18 +17,7 @@ module.exports = function(config) {
     reporters: ['dots'],
     webpack: {
       devtool: 'inline-source-map',
-      module: {
-        loaders: [
-          {
-            test: /\.jade$/,
-            loader: "jade"
-          },
-          {
-            test: /\.sass$/,
-            loaders: ["style", "css", "sass"]
-          }
-        ]
-      }
+      module: webpackConfig.module
     },
     webpackServer: {
       noInfo: true
