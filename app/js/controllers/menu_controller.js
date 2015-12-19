d36a7785b4950efe18048500c68fb15463ba4ca0
Jun <felipe.jun.ohira@gmail.com>
2015-12-19 14:38:53 -0200

    Changing the ViewLoader usage

--- app/js/controllers/menu_controller.js
+++ app/js/controllers/menu_controller.js
@@ -1,13 +1,11 @@
 var Screen = require('../../lib/screen').getInstance();
 var ViewLoader = require('../../lib/view_loader');
 
-var $ = require('jquery');
-
 module.exports = (function() {
-  var viewLoader = new ViewLoader();
+  var viewLoader = new ViewLoader('app/js/views/menu');
 
   function MenuController() {
-    var loader = viewLoader.loadView('menu');
+    var loader = viewLoader.load();
     this._content = loader();
     Screen.addContent(this._content);
   };
