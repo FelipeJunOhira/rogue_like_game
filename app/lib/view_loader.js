cb9f5bef352f0a598a3891bafe22d0b405ced182
Jun <felipe.jun.ohira@gmail.com>
2015-12-19 14:06:21 -0200

    Testing ViewLoader load method

--- app/lib/view_loader.js
+++ app/lib/view_loader.js
@@ -1,6 +1,15 @@
 module.exports = (function() {
 
-  function ViewLoader() {
+  function ViewLoader(basePath) {
+    this.basePath = basePath;
+  };
+
+  ViewLoader.prototype.load = function(relativePath) {
+    return require('../../' + this._getPath(relativePath) + '.jade');
+  };
+
+  ViewLoader.prototype._getPath = function(relativePath) {
+    return this.basePath + '/' + relativePath;
   };
 
   ViewLoader.prototype.loadTemplate = function(templateName) {
--- tests/lib/view_loader_test.js
+++ tests/lib/view_loader_test.js
@@ -3,7 +3,21 @@ var expect = require('chai').expect;
 var ViewLoader = require('app/lib/view_loader');
 
 describe('ViewLoader', function() {
+  var viewLoader;
+
   it('can be instantiated', function() {
     expect(new ViewLoader()).to.be.instanceof(ViewLoader);
+  });
+
+  describe('.load', function() {
+    beforeEach(function() {
+      viewLoader = new ViewLoader('tests/fixtures');
+    });
+
+    it('returns a HTML compiler function', function() {
+      var compiler = viewLoader.load('template');
+
+      expect(compiler()).to.be.equal('<div>Test Success!</div>');
+    });
   });
 });
