9fa5f2c58b60394afedb920d2acdfaa0fdb5f69e
Jun <felipe.jun.ohira@gmail.com>
2016-01-17 23:14:07 -0200

    Handling the keyboard events on the menu controller

--- src/js/controllers/menu_controller.js
+++ src/js/controllers/menu_controller.js
@@ -1,7 +1,13 @@
 var HighScoreController = require('./high_score_controller');
 var BaseController = require('./base_controller');
 
+var ButtonGroup = require('../views/components/button_group');
+
+var ButtonGroupManager = require('./menu_controller/button_group_manager');
+
 module.exports = (function() {
+  var START_GAME_BUTTON_INDEX = 0;
+  var HIGH_SCORE_BUTTON_INDEX = 1;
 
   function MenuController() {};
 
@@ -12,18 +18,37 @@ module.exports = (function() {
 
   MenuController.prototype.beforeLoadView = function() {
     this._bindViewElements();
-    this._bindViewEvents();
   };
 
   MenuController.prototype._bindViewElements = function() {
     var content = this.getContent();
-    this.startGameButton = content.find('#start-game-button');
-    this.highScoreButton = content.find('#high-score-button');
+    var buttonGroupContent = content.find('#button-group');
+    var buttonGroup = new ButtonGroup(buttonGroupContent);
+    this.buttonGroupManager = new ButtonGroupManager(buttonGroup);
   };
 
-  MenuController.prototype._bindViewEvents = function() {
-    this.startGameButton.click(this.onStartGameButtonClick.bind(this));
-    this.highScoreButton.click(this.onHighScoreButtonClick.bind(this));
+  MenuController.prototype.onKeyUpPressed = function() {
+    this.buttonGroupManager.selectPreviousButton();
+  };
+
+  MenuController.prototype.onKeyDownPressed = function() {
+    this.buttonGroupManager.selectNextButton();
+  };
+
+  MenuController.prototype.onKeyEnterPressed = function() {
+    this._dispatchButtonClickedEvent();
+  };
+
+  MenuController.prototype._dispatchButtonClickedEvent = function() {
+    var selectedIndex = this.buttonGroupManager.getSelectedIndex();
+    switch (selectedIndex) {
+      case START_GAME_BUTTON_INDEX:
+        this.onStartGameButtonClick();
+        break;
+      case HIGH_SCORE_BUTTON_INDEX:
+        this.onHighScoreButtonClick();
+        break;
+    }
   };
 
   MenuController.prototype.onStartGameButtonClick = function() {
